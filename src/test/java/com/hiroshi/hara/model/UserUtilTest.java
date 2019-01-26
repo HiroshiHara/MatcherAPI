598bc6ec36fa81c654fdbb576e04805980a53284
HaraHiroshi <mrbob403@gmail.com>
2019-01-27 00:57:44 +0900

    typo修正

--- src/test/java/com/hiroshi/hara/model/UserUtilTest.java
+++ src/test/java/com/hiroshi/hara/model/UserUtilTest.java
@@ -1,8 +1,8 @@
 package com.hiroshi.hara.model;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.nullValue;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
@@ -18,15 +18,15 @@ import com.hiroshi.hara.beans.User;
  */
 
 public class UserUtilTest {
-	
+
 	/*
 	 * テスト対象クラスのアクセス修飾子がprivateなので、各テストメソッドの記述には以下を参考にすること
 	 * 
 	 */
-	
+
 	private static String CLASS_NAME = "com.hiroshi.hara.model.UserUtil";
 	private static Object object;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		try {
@@ -38,24 +38,23 @@ public class UserUtilTest {
 			fail(e.getMessage());
 		}
 	}
-	
+
 	/*
-	 * protected, privateメソッドのテスト実施…reflectionを用いる
-	 * Sample sample = new Sample();
+	 * protected, privateメソッドのテスト実施…reflectionを用いる Sample sample = new Sample();
 	 * Method method = Sample.class.getDeclaredMethod("<メソッド名>", 引数の型1, 引数の型2...);
-     * method.setAccessible(true);
-     * int actual = (戻り値の型)method.invoke(<インスタンス>,引数1,引数2...);
+	 * method.setAccessible(true); int actual =
+	 * (戻り値の型)method.invoke(<インスタンス>,引数1,引数2...);
 	 */
-	
+
 	@Test
 	public void isNullThisUserでnullを渡したときにtrueが得られるかどうか() throws Exception {
 		// SetUp
 		User user = null;
-		UserUtil sut = (UserUtil)object;
-		Method method =  UserUtil.class.getDeclaredMethod("isNullThisUser", User.class);
+		UserUtil sut = (UserUtil) object;
+		Method method = UserUtil.class.getDeclaredMethod("isNullThisUser", User.class);
 		method.setAccessible(true);
 		// Exercise
-		Boolean actual = (Boolean)method.invoke(sut, user);
+		Boolean actual = (Boolean) method.invoke(sut, user);
 		// Verify
 		assertThat(actual, is(true));
 	}
